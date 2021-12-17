a87c0bec0a8db7ecf243f294b077a4c440bc817a
alexsmik <alexsmik@yandex.ru>
2021-12-17 13:42:31 +0300

    add check

--- authController.js
+++ authController.js
@@ -4,9 +4,14 @@ let Role;
 Role = require('./model/Role');
 let bcrypt;
 bcrypt = require('bcryptjs');
+const {validationResult} = require('express-validator');
 class AuthController {
     async registration(req, res) {
         try{
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({message: "error when reg"}, errors)
+            }
             const {username, password} = req.body;
             const candidate = await User.findOne({username});
             if (candidate) {
--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,13 @@
 const Router = require('express');
 const router = new Router();
 const controller = require('./authController');
+const {check} = require('express-validator');
 
-router.post('/registration', controller.registration);
+router.post('/registration',[
+    check('username', "empty").notEmpty(),
+    check('password', "more than 4").isLength({min:4,max:10})
+],controller.registration);
+
 router.post('/login', controller.login);
 router.get('/users', controller.getUser);
 
